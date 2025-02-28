aa712c2b38c63fb76710c3b9ef44271e37d50a7f
anonimus22848 <ssymonik7@gmail.com>
2025-02-28 20:32:28 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,11 +7,9 @@ int main()
  cout << "hello world"<< endl;
  cout << "hello world"<< endl;
  cout << "hello world"<< endl;
+cout << "hello world"<< endl;
+cout << "hello world"<< endl;
 
-
-
- cout << "hello worldddddd"<< endl;
- cout << "hello worldddddd"<< endl;
 
  return 0;
 } 
